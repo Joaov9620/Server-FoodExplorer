95659e6e55f26dafe9ac3aec478be6c239e3b1e6
joao <pjoaovictor22@hotmail.com>
2023-04-22 15:38:51 -0300

    atp

--- src/server.js
+++ src/server.js
@@ -4,6 +4,9 @@ const AppError = require('./utils/AppError');
 const express = require('express');
 const app = express();
 
+const cors = require('cors');
+app.use(cors());
+
 app.use(express.json());
 
 const routes = require('./routes');
