ff4b42f0a31fb412429de636d96e749dd17da4db
joao <pjoaovictor22@hotmail.com>
2023-05-19 20:22:11 -0300

    comit

--- src/server.js
+++ src/server.js
@@ -36,5 +36,5 @@ app.use((error, req, res, next) => {
     })
 });
 
-const PORT = process.env.PORT || 3332;
+const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
