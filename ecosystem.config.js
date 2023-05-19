c7e02dc7908656e984a5cc3767665d4ea67f4b85
joao <pjoaovictor22@hotmail.com>
2023-05-19 19:56:14 -0300

    comit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,13 @@
 module.exports = {
   apps : [{
-    script: './src/server.js',
-    watch: '.'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
-  }],
-
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
+    name: "app",
+    script: "./src/server.js",
+    instances: "max",
+    env: { 
+      NODE_ENV: "development",
+    },
+    env_production: {
+      NODE_ENV: "production",
     }
-  }
-};
+  }]
+}
